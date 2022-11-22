61be0a82243ff94e9280edfd70b6339dfe400474
edmariooliver <barrigacraft@hotmail.com>
2022-11-22 15:02:17 -0300

    autenticando rotas

--- routes/api.php
+++ routes/api.php
@@ -34,33 +34,38 @@ use App\Http\Controllers\Api\Invoice\{
 };
 
 // ===============================================
+Route::group([
+    'middleware' => 'auth.jwt',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::get("user/{id}",         [GetUserController::class, "findById"]);
+    Route::get("user",              [GetUserController::class, "findAll"]);
+    Route::patch("user/{id}",       [UpdateUserController::class, "update"]);
+    Route::delete("user/{id}",      [DestroyUserController::class, "destroy"]);
+    
+    Route::get("club/{id}",         [GetClubController::class, "findById"]);
+    Route::get("club",              [GetClubController::class, "findAll"]);
+    Route::patch("club/{id}",       [UpdateClubController::class, "update"]);
+    Route::delete("club/{id}",      [DestroyClubController::class, "destroy"]);
+    Route::post("club",             [CreateClubController::class, "create"]);
+    
+    Route::get("signature/checkAll",     [CheckSignaturesController::class, "index"]);
+    Route::get("signature/{id}",         [GetSignatureController::class, "findById"]);
+    Route::get("signature",              [GetSignatureController::class, "findAll"]);
+    Route::patch("signature/{id}",       [UpdateSignatureController::class, "update"]);
+    Route::delete("signature/{id}",      [DestroySignatureController::class, "destroy"]);
+    Route::post("signature",             [CreateSignatureController::class, "create"]);
+    
+    Route::post("invoice/pay/{id}",      [PayInvoiceController::class, "pay"]);
+    Route::get("invoice/checkAll",       [CheckInvoicesController::class, "index"]);
+    
+    Route::post('logout',       [LogoutController::class, "index"]);
+    Route::post('refresh',      [RefreshController::class, "index"]);
+    Route::post('me',           [MeController::class, "index"]);
+    Route::post("user",         [CreateUserController::class, "create"]);
+});
 
 Route::post('auth/login',   [LoginController::class, "index"]);
-Route::post("user",         [CreateUserController::class, "create"]);
-Route::post('logout',       [LogoutController::class, "index"]);
-Route::post('refresh',      [RefreshController::class, "index"]);
-Route::post('me',           [MeController::class, "index"]);
 Route::post('register',     [RegisterController::class, "create"]);
 
-Route::get("user/{id}",         [GetUserController::class, "findById"]);
-Route::get("user",              [GetUserController::class, "findAll"]);
-Route::patch("user/{id}",       [UpdateUserController::class, "update"]);
-Route::delete("user/{id}",      [DestroyUserController::class, "destroy"]);
-
-
-Route::get("club/{id}",         [GetClubController::class, "findById"]);
-Route::get("club",              [GetClubController::class, "findAll"]);
-Route::patch("club/{id}",       [UpdateClubController::class, "update"]);
-Route::delete("club/{id}",      [DestroyClubController::class, "destroy"]);
-Route::post("club",             [CreateClubController::class, "create"]);
-
-Route::get("signature/checkAll",     [CheckSignaturesController::class, "index"]);
-Route::get("signature/{id}",         [GetSignatureController::class, "findById"]);
-Route::get("signature",              [GetSignatureController::class, "findAll"]);
-Route::patch("signature/{id}",       [UpdateSignatureController::class, "update"]);
-Route::delete("signature/{id}",      [DestroySignatureController::class, "destroy"]);
-Route::post("signature",             [CreateSignatureController::class, "create"]);
-
-Route::post("invoice/pay/{id}",      [PayInvoiceController::class, "pay"]);
-Route::get("invoice/checkAll",       [CheckInvoicesController::class, "index"]);
 
