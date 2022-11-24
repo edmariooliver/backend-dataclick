2749d86daa4148b5a7f1ac6a6f5966f06d3ecd56
edmariooliver <barrigacraft@hotmail.com>
2022-11-24 14:16:12 -0300

    seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
         DB::table('users')->insert([
             'name' => "marrios",
             'email' => "marrios@gmail.com",
